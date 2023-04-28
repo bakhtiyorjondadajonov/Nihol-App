63de943b763b69692a081673a53e323363108622
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2023-04-28 17:09:36 +0900

    finished until saving url in env file

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,17 +1,38 @@
-import React, { useRef } from 'react'
-import { LoginBtn, Password, PhoneNumber, Wrapper } from './style'
+import React, { useRef, useState } from 'react'
 
-function Login() {
+import { LoginBtn, Password, PhoneNumber, Wrapper } from './style'
+import {LoadingOutlined} from "@ant-design/icons"
+import axios from 'axios'
+ function Login() {
   const phoneNumberRef=useRef()
   const passwordRef=useRef()
+  const [loading,setLoading]=useState(false)
 
-    
-   
-    const onAuth=()=>{
-        const userData={
-            phoneNumber:phoneNumberRef.current.input.value,
+  const onKeyDetect=(e)=>{
+
+    if(e.key==="Enter" || e.type==="click"){
+      onAuth()
+    }
+      }
+    const onAuth=async ()=>{
+        const userValue={
+            phoneNumber:`+998${phoneNumberRef.current.input.value}`,
             password:passwordRef.current.input.value
         }
+        try {
+          const response=await axios({
+            url:`${process.env.REACT_APP_MAIN_URL}/user/sign-in`,
+            method:"POST",
+            data:{
+              ...userValue
+            }
+           })
+           // ------- N
+        } catch (error) {
+          console.log(error)
+        }
+      
+      
     }
   return (
     <Wrapper>
@@ -19,8 +40,8 @@ function Login() {
     <Wrapper.Title> Yana bir bor Salom👋</Wrapper.Title>
     <Wrapper.Description>Biz har kuni kechagidan ko'ra yaxshiroq xizmat ko'rsatishga intilamiz.</Wrapper.Description>
 <PhoneNumber ref={phoneNumberRef} name="phoneNumber"  addonBefore="+998" size="large" bordered={false} placeholder='Enter your number...'/>
-<Password ref={passwordRef} name="password"   size="large"></Password>
-<LoginBtn onClick={onAuth} type='primary'>Login</LoginBtn>
+<Password onKeyDown={onKeyDetect} ref={passwordRef} name="password"   size="large"></Password>
+<LoginBtn onClick={onKeyDetect} type='primary'> {loading?<LoadingOutlined />:"Login"} </LoginBtn>
       </Wrapper.Container>
     </Wrapper>
   )
